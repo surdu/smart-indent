deb94979d361860a242edfb3f7d5a5a88aee16d9
surdu <postal2600@gmail.com>
2018-04-08 19:29:29 +0300

    Fixed a couple of issues

--- spec/smart-indent-spec.js
+++ spec/smart-indent-spec.js
@@ -3,6 +3,14 @@ describe("smart-indent", function () {
 	var editor;
 	var editorView;
 
+	function test(inputText, expectedText) {
+		editor.setText(inputText);
+		editor.moveToEndOfLine();
+		atom.commands.dispatch(editorView, 'editor:newline');
+
+		expect(editor.getText()).toBe(expectedText);
+	}
+
 	beforeEach(function () {
 		waitsForPromise(function () {
 			return atom.workspace.open('test');
@@ -26,20 +34,12 @@ describe("smart-indent", function () {
 	});
 
 	it("smart indents if new prev line's identation level is the same", function () {
-		editor.setText("\t\t    test");
-		editor.moveToEndOfLine();
-		atom.commands.dispatch(editorView, 'editor:newline');
-
-		expect(editor.getText()).toBe("\t\t    test\n\t\t    ");
+		test("\t\t    test", "\t\t    test\n\t\t    ");
 	});
 
 
 	it("smart idents if previous line align spaces count don't divide by tabLength", function () {
-		editor.setText("\t\t   test");
-		editor.moveToEndOfLine();
-		atom.commands.dispatch(editorView, 'editor:newline');
-
-		expect(editor.getText()).toBe("\t\t   test\n\t\t   ");
+		test("\t\t   test", "\t\t   test\n\t\t   ");
 	});
 
 	it("ignores alignment when indent level will be increased", function () {
@@ -61,6 +61,14 @@ describe("smart-indent", function () {
 				expect(editor.getText()).toBe("if (ceva &&\n\t\t    altceva) {\n\t\t\t");
 			});
 		});
+	});
+
+	it("smart idents when previous line starts directly with alignment", function () {
+		test("    test", "    test\n    ");
+	});
+
+	it("shoul not consider spaces other than alignment spaces", function () {
+		test("some   string", "some   string\n");
 	});
 
 });
